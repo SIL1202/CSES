33d5123cbeaba092ad993816a1bd6a2f3605d4fd
劉家均 <steven60344@gmail.com>
2025-03-17 13:42:44 +0800

    add: dice combination

--- Dynamic Programming/Dice combination.cpp
+++ Dynamic Programming/Dice combination.cpp
@@ -2,12 +2,33 @@
 using namespace std;
 #define mod (int)(1e9 + 7)
 
-int dp[10000001]; // 全域變數初始化為0
+// Top-Down
+int ways(int n) {
+  if (n == 0)
+    return 1;
+
+  int result = 0;
+  for (int i = 1; i <= 6; i++) {
+    if (n < i)
+      break;
+    result = (result + ways(n - i)) % mod;
+  }
+  return result;
+}
+
+int main() {
+  int n;
+  cin >> n;
+  cout << ways(n);
+}
+
+// Bottom-Up
+/* int dp[10000001]; // 全域變數初始化為0
 int main() {
   // fill(dp, dp + 10000001, 0); // 內顯式設定初始化
   int n;
   cin >> n;
-  dp[0] = 1; // base case
+  up[0] = 1; // base case
   for (int i = 1; i <= n; i++) {
     for (int j = 1; j <= 6; j++) {
       if (j > i)
@@ -17,12 +38,11 @@ int main() {
   }
   cout << dp[n];
   return 0;
-}
+} */
 
 /*
 
 // if only dice 1, 3, 4
-
 dp[0] = 1;
 for (int i = 0; i <= n; i++) {
   if (i >= 1)
