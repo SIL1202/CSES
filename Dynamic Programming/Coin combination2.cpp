2a44a8f3eb6e17ea06347fe52aa542e0a8bdef75
SIL1202 <167217913+SIL1202@users.noreply.github.com>
2025-06-05 11:24:00 +0800

    fix: improve coin combination2 solution with dynamic programming

--- Dynamic Programming/Coin combination2.cpp
+++ Dynamic Programming/Coin combination2.cpp
@@ -3,6 +3,25 @@
 using namespace std;
 #define mod (int)(1e9 + 7)
 
+// solve 2
+void solve2(int a[], int n, int x) {
+  vector<vector<int>> dp(n + 1, vector<int>(x + 1, 0));
+
+  // base case: amount 0 can always be formed with no coins
+  for (int i = 0; i <= n; ++i)
+    dp[i][0] = 1;
+
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 0; j <= x; ++j) {
+      dp[i][j] = dp[i - 1][j]; // 不使用第 i 個硬幣的情況
+      if (j >= a[i - 1])
+        dp[i][j] = (dp[i][j] + dp[i][j - a[i - 1]]) % mod;
+    }
+  }
+
+  cout << dp[n][x];
+}
+
 int main() {
   int n, x;
   cin >> n >> x;
@@ -10,14 +29,19 @@ int main() {
   for (int i = 0; i < n; i++)
     cin >> a[i];
 
+  // solve 1
+  /*
   vector<int> dp(x + 1, 0);
   dp[0] = 1;
+
   for (int j = 0; j < n; j++) {
-    for (int i = 1; i <= x; i++) {
-      if (a[j] <= i)
-        dp[i] = (dp[i] + dp[i - a[j]]) % mod;
+    for (int i = a[j]; i <= x; i++) {
+      dp[i] = (dp[i] + dp[i - a[j]]) % mod;
     }
   }
   cout << dp[x];
+  */
+
+  solve2(a, n, x);
   return 0;
 }
