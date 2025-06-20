9e5c679f5df05a4a4e467095916892e72185480a
SIL1202 <167217913+SIL1202@users.noreply.github.com>
2025-06-21 01:17:10 +0800

    feat: implement Dijkstra's algorithm using priority_queue for shortest paths

--- Graph Algorithm/Shortest Routes I.cpp
+++ Graph Algorithm/Shortest Routes I.cpp
@@ -1,17 +1,45 @@
 #include <iostream>
+#include <queue>
 #include <utility>
+#include <vector>
+
+#define ll long long
+const ll INF = 1e18;
 using namespace std;
 const int MAX = 1e5;
-int n, m;
-vector<pair<int, int>> adj[MAX];
 
 int main() {
+  int n, m;
   cin >> n >> m;
-  for (int i = 0; i < n; i++) {
+  vector<pair<int, int>> adj[n + 1];
+
+  for (int i = 0; i < m; i++) {
     int a, b, c;
     cin >> a >> b >> c;
-    adj[a].push_back({b, c});
+    adj[a].emplace_back(b, c);
   }
 
+  // {dist, node}
+  priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<>> pq;
+  vector<ll> dist(n + 1, INF);
+  dist[1] = 0;
+  pq.emplace(0, 1);
+
+  while (!pq.empty()) {
+    auto [d, u] = pq.top();
+    pq.pop();
+    if (d > dist[u])
+      continue;
+
+    for (const auto &[v, w] : adj[u]) {
+      if (dist[v] > dist[u] + w) {
+        dist[v] = dist[u] + w;
+        pq.emplace(dist[v], v);
+      }
+    }
+  }
+
+  for (int i = 1; i <= n; i++)
+    cout << dist[i] << ' ';
   return 0;
 }
